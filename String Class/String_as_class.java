683581f459cbac2f797fd8ec7f9ae06f6f5fdb6e
Mahesh_X_18 <maheshpatil18182424@gmail.com>
2026-04-05 22:44:35 +0530

    Refactor String_as_class: replace String with StringBuffer for enhanced string manipulation

--- String Class/String_as_class.java
+++ String Class/String_as_class.java
@@ -2,11 +2,30 @@
 public class String_as_class
 {
   public static void main(String[] args) {
-    String name = new String("Mahesh");
-    System.out.println(name);
-    System.out.println(name +", Hello");
-    System.out.println(name.charAt(3));
-    System.out.println(name.concat("(Doron)"));
-    System.out.println(name.hashCode());
+
+    StringBuffer sb = new StringBuffer("Mahesh");
+    System.out.println(sb.capacity());
+    System.out.println(sb.length());
+    System.out.println(sb.append("Patil"));
+    System.out.println(sb);
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // String name = new String("Mahesh");
+    // System.out.println(name);
+    // System.out.println(name +", Hello");
+    // System.out.println(name.charAt(3));
+    // System.out.println(name.concat("(Doron)"));
+    // System.out.println(name.hashCode());
   }
 }
