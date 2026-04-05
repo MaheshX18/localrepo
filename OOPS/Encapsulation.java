a22f9bee0817efc3575a30ed58ff7035e3da5b1e
Mahesh_X_18 <maheshpatil18182424@gmail.com>
2026-04-05 23:01:17 +0530

    Add Encapsulation class: implement Human class with getter and setter methods for age and name

---
+++ OOPS/Encapsulation.java
@@ -0,0 +1,57 @@
+package OOPS;
+
+class Human
+{
+  private int age = 118;
+  private String name = "Mahesh";
+  
+  public int getAge() {
+    return age;
+  }
+  public void setAge(int age) {
+    this.age = age;
+  }
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  
+
+  // public int getAge()
+  // {
+  //   return age;
+  // }
+
+  // public String getName()
+  // {
+  //   return name;
+  // }
+
+  // public void setAge(int a)
+  // {
+  //   age = a;
+  // }
+  // public void setName(String n)
+  // {
+  //   name = n;
+  // }
+}
+public class Encapsulation
+{
+  public static void main(String[] args) {
+    
+    Human obj = new Human();
+
+    // obj.age = 19;
+    // obj.name = "Doron";
+
+    System.out.println(obj.getAge()+" "+obj.getName());
+    obj.setAge(12);
+    System.out.println(obj.getAge()+" "+obj.getName());
+  }
+
+
+}
