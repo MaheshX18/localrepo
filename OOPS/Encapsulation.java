65ee4dbd289d914f46296032a0dc56e7a49dd38f
Mahesh_X_18 <maheshpatil18182424@gmail.com>
2026-04-05 23:24:01 +0530

    Refactor Human class: add constructor for initialization and clean up code

--- OOPS/Encapsulation.java
+++ OOPS/Encapsulation.java
@@ -4,7 +4,17 @@ class Human
 {
   private int age = 118;
   private String name = "Mahesh";
+
+
+  public Human() {
+  }
   
+  public Human(int age, String name) {
+    this.age = age;
+    this.name = name;
+  }
+  
+
   public int getAge() {
     return age;
   }
@@ -44,12 +54,17 @@ public class Encapsulation
   public static void main(String[] args) {
     
     Human obj = new Human();
+    Human obj1 = new Human();
 
     // obj.age = 19;
     // obj.name = "Doron";
 
     System.out.println(obj.getAge()+" "+obj.getName());
+
     obj.setAge(12);
+    obj.setName("Doron");
+
+
     System.out.println(obj.getAge()+" "+obj.getName());
   }
 
